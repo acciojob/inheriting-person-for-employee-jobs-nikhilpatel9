396809b0426f8edb01a68deebaab8e0971b27873
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-08-15 13:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name, age) {
+    this.username = name; 
+    this.userAge = age;
+  }
+	greet(){
+			console.log(`Hello, my name is ${name}, I am ${age} years old.`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	super(name,age);
+	this.jobTitle=jobTitle;
+  }
+	jobGreet(){
+		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`)
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
